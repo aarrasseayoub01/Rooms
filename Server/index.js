33ada29b9252a347ae53572e0e3ada3c79ea08c1
oussamaZAAM <zaam.oussama@gmail.com>
2022-06-03 12:13:11 +0200

    heroku it

--- Server/index.js
+++ Server/index.js
@@ -52,7 +52,7 @@ const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static('Rooms-oussama/build'));
 }
 
 //Se connecter a la base de donner puis rendre le serveur de backend accessible.
